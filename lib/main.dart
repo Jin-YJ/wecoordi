5691e4c349f39caa0d2938df015b1abfa19b9adf
진영준 <jin-yeongjun@jin-yeongjun-ui-MacBookPro.local>
2023-07-25 14:03:52 +0900

    프로젝트 커밋

---
+++ lib/main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: wecoordiHome(),
+    );
+  }
+}
+
+
+class wecoordiHome extends StatelessWidget {
+  const wecoordiHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(leading: IconButton(
+          icon: Icon(CupertinoIcons.add, color: Colors.black),
+          onPressed: () {},
+          
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(CupertinoIcons.paperplane, color: Colors.black),
+            onPressed: () {},
+          )],
+          backgroundColor: Colors.white,
+         title: Text(
+    'wecoordi',
+    style: TextStyle(
+      fontFamily: 'assets/fonts/logo_font.ttf', // 폰트 파일의 경로를 지정
+      fontSize: 20,
+      fontWeight: FontWeight.bold,
+      color: Colors.black,
+    ),
+  ),
+      )
+    ); 
+  }
+} 
